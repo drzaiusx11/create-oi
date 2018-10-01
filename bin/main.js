a4be5bbc97eee4fd7de2c69d45ab104930116e50
Jeff Rousseau <jrousseau@athenahealth.com>
2018-10-01 16:25:05 -0400

    fix baudrate error

--- bin/main.js
+++ bin/main.js
@@ -215,7 +215,7 @@ var create = (function() {
 
     module.init = function(settings) {
         serial = new SerialPort(settings.serialport, {
-          baudrate: settings.version === 2 ? create2Baudrate : create1Baudrate, 
+          baudRate: settings.version === 2 ? create2Baudrate : create1Baudrate, 
           bufferSize: 5
         });
 
